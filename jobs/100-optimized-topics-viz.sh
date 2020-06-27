f938943697754725c2a926a219afc3ef6f19d0bd
Ryan Shaw <ryanshaw@unc.edu>
2020-06-26 20:18:00 -0400

    keep job scripts in git

---
+++ jobs/100-optimized-topics-viz.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -p general
+#SBATCH -N 1
+#SBATCH --mem 64G
+#SBATCH -n 16
+#SBATCH -t 4:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ryanshaw@email.unc.edu
+
+module add make/4.3
+module add python/3.8.3
+module add java/10.0.2
+module add apache-ant/1.10.8
+module add go/1.14.4
+
+cd "$HOME/dth-topics" || exit
+MEMORY=64g \
+CPUS=16 \
+SCRATCH="$HOME/scratch" \
+make viz/100-optimized-topics/index.html
---
+++ jobs/200-topics-viz.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -p general
+#SBATCH -N 1
+#SBATCH --mem 256G
+#SBATCH -n 16
+#SBATCH -t 6:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ryanshaw@email.unc.edu
+
+module add make/4.3
+module add python/3.8.3
+module add java/10.0.2
+module add apache-ant/1.10.8
+module add go/1.14.4
+
+cd "$HOME/dth-topics" || exit
+MEMORY=256g \
+CPUS=16 \
+SCRATCH="$HOME/scratch" \
+make viz/200-topics/index.html
