f9de8451915ad1465679068556a94907fc0491a0
Ryan Shaw <ryanshaw@unc.edu>
2020-06-29 16:46:53 -0400

    give 200-optimized 200G

--- jobs/200-optimized-topics-viz.sh
+++ jobs/200-optimized-topics-viz.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p general
 #SBATCH -N 1
-#SBATCH --mem 150G
+#SBATCH --mem 200G
 #SBATCH -n 16
 #SBATCH -t 8:00:00
 #SBATCH --mail-type=ALL
@@ -15,7 +15,7 @@ module add apache-ant/1.10.8
 module add go/1.14.4
 
 cd "$HOME/dth-topics" || exit
-MEMORY=150g \
+MEMORY=200g \
 CPUS=16 \
 SCRATCH="$HOME/scratch" \
 make viz/200-optimized-topics/index.html
